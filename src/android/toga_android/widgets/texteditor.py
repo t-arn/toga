40387720554ba7b15a33ed9fecf003535067bb48
sw@t-arn.com <sw@t-arn.com>
2022-09-25 01:11:41 +0200

    Added scrolling and line numbers

--- src/android/toga_android/widgets/texteditor.py
+++ src/android/toga_android/widgets/texteditor.py
@@ -3,10 +3,17 @@ from travertino.size import at_least
 
 from toga_android.colors import native_color
 
+from ..libs.android.graphics import Typeface
 from ..libs.android.text import InputType, TextWatcher
 from ..libs.android.util import TypedValue
 from ..libs.android.view import Gravity
-from ..libs.android.widget import EditText
+from ..libs.android.widget import (
+    EditText,
+    HorizontalScrollView,
+    LinearLayout,
+    LinearLayout__LayoutParams,
+    ScrollView
+)
 from .base import Widget, align
 
 
@@ -20,6 +27,8 @@ class TogaTextWatcher(TextWatcher):
         pass
 
     def afterTextChanged(self, _editable):
+        if self.interface.linenumbers:
+            self.impl.generate_linenumbers()
         if self.interface.on_change:
             self.interface.on_change(widget=self.interface)
 
@@ -29,51 +38,101 @@ class TogaTextWatcher(TextWatcher):
 
 class TextEditor(Widget):
     def create(self):
+        self._et_numbers = None
+        self._prev_linecount = -1
+        self._et_text = None
         self._textChangedListener = None
-        self.native = EditText(self._native_activity)
-        self.native.setInputType(
+        self.native = LinearLayout(self._native_activity)
+        self.native.setOrientation(LinearLayout.VERTICAL)
+        mm_params = LinearLayout__LayoutParams(
+            LinearLayout__LayoutParams.MATCH_PARENT,
+            LinearLayout__LayoutParams.MATCH_PARENT
+        )  # width, height
+        mw_params = LinearLayout__LayoutParams(
+            LinearLayout__LayoutParams.MATCH_PARENT,
+            LinearLayout__LayoutParams.WRAP_CONTENT
+        )  # width, height
+        self.native.setLayoutParams(mm_params)
+        vsv = ScrollView(self._native_activity)
+        vsv.setFillViewport(True)
+        vsv.setLayoutParams(mm_params)
+        ll_editor = LinearLayout(self._native_activity)
+        vsv.addView(ll_editor)
+        if self.interface.linenumbers:
+            self._et_numbers = EditText(self._native_activity)
+            self.set_alignment(self._et_numbers, RIGHT)
+            self._et_numbers.setTypeface(Typeface.MONOSPACE)
+            self._et_numbers.setText("1")
+            self._et_numbers.setEnabled(False)
+            ll_editor.addView(self._et_numbers)
+        hsv = HorizontalScrollView(self._native_activity)
+        hsv.setLayoutParams(mw_params)
+        hsv.setFillViewport(True)
+        ll_editor.addView(hsv)
+        self._et_text = EditText(self._native_activity)
+        self.set_alignment(self._et_text, LEFT)
+        self._et_text.setTypeface(Typeface.MONOSPACE)
+        self._et_text.setInputType(
             InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_MULTI_LINE
         )
-        # Set default alignment
-        self.set_alignment(LEFT)
+        self._et_text.setText("")
+        hsv.addView(self._et_text)
+        self.native.addView(vsv)
+        self._et_text.requestFocus()
 
+    def generate_linenumbers(self):
+        line_count = self._et_text.getLineCount()
+        if line_count != self._prev_linecount:
+            lines = "1"
+            for x in range(2, line_count + 1):
+                lines += "\n" + str(x)
+            self.set_value(lines)
+            self._prev_linecount = line_count
+    
     def get_value(self):
-        return self.native.getText().toString()
+        return self._et_text.getText().toString()
 
     def set_readonly(self, value):
-        self.native.setFocusable(not value)
+        self._et_text.setFocusable(not value)
 
     def set_placeholder(self, value):
         # Android EditText's setHint() requires a Python string.
-        self.native.setHint(value if value is not None else "")
+        self._et_text.setHint(value if value is not None else "")
 
-    def set_alignment(self, value):
+    def set_alignment(self, obj, value):
         # Refuse to set alignment unless widget has been added to a container.
         # This is because Android EditText requires LayoutParams before
         # setGravity() can be called.
         if not self.native.getLayoutParams():
             return
-        self.native.setGravity(Gravity.TOP | align(value))
+        obj.setGravity(Gravity.TOP | align(value))
 
     def set_color(self, color):
         if color:
-            self.native.setTextColor(native_color(color))
+            self._et_text.setTextColor(native_color(color))
 
     def set_font(self, font):
         if font:
             font_impl = font.bind(self.interface.factory)
-            self.native.setTextSize(TypedValue.COMPLEX_UNIT_SP, font_impl.get_size())
-            self.native.setTypeface(font_impl.get_typeface(), font_impl.get_style())
+            self._et_text.setTextSize(TypedValue.COMPLEX_UNIT_SP, font_impl.get_size())
+            self._et_text.setTypeface(font_impl.get_typeface(), font_impl.get_style())
 
     def set_value(self, value):
-        self.native.setText(value)
+        self._et_text.setText(value)
 
     def set_on_change(self, handler):
         if self._textChangedListener:
-            self.native.removeTextChangedListener(self._textChangedListener)
+            self._et_text.removeTextChangedListener(self._textChangedListener)
         self._textChangedListener = TogaTextWatcher(self)
-        self.native.addTextChangedListener(self._textChangedListener)
+        self._et_text.addTextChangedListener(self._textChangedListener)
 
     def rehint(self):
-        self.interface.intrinsic.width = at_least(self.interface.MIN_WIDTH)
-        self.interface.intrinsic.height = at_least(self.interface.MIN_HEIGHT)
+        # Android can crash when rendering some widgets until they have their layout params set. Guard for that case.
+        if not self.native.getLayoutParams():
+            return
+        self.native.measure(
+            View__MeasureSpec.UNSPECIFIED,
+            View__MeasureSpec.UNSPECIFIED,
+        )
+        self.interface.intrinsic.width = at_least(self.native.getMeasuredWidth())
+        self.interface.intrinsic.height = self.native.getMeasuredHeight()
--- src/core/toga/widgets/texteditor.py
+++ src/core/toga/widgets/texteditor.py
@@ -13,11 +13,14 @@ class TextEditor(Widget):
         style(:obj:`Style`):  An optional style object.
             If no style is provided then a new one will be created for the widget.
         factory: Optional factory that must be able to return a implementation
-            of a MulitlineTextInput Widget.
+            of a TextEditor Widget.
         value (str): The initial text of the widget.
         readonly (bool): Whether a user can write into the text input,
             defaults to `False`.
         placeholder (str): The placeholder text for the widget.
+        on_change (``callable``): The handler to invoke when the text changes.
+        linenumbers (bool): Whether line numbers should be shown,
+            defaults to `True`
     """
     MIN_HEIGHT = 100
     MIN_WIDTH = 100
@@ -30,7 +33,8 @@ class TextEditor(Widget):
         value=None,
         readonly=False,
         placeholder=None,
-        on_change=None
+        on_change=None,
+        linenumbers=True
     ):
         super().__init__(id=id, style=style, factory=factory)
 
@@ -38,9 +42,12 @@ class TextEditor(Widget):
         self._impl = self.factory.TextEditor(interface=self)
 
         # Set all the properties
+        # Set the initial value before on_change because we do not want it
+        # to trigger on_change
         self.value = value
         self.readonly = readonly
         self.placeholder = placeholder
+        self.linenumbers = linenumbers
         self.on_change = on_change
 
     @property
@@ -59,7 +66,7 @@ class TextEditor(Widget):
 
     @property
     def readonly(self):
-        """ Whether a user can write into the text input
+        """ Whether a user can write into the editor
 
         Returns:
             `True` if the user can only read, `False` if the user can read and write the text.
@@ -73,10 +80,10 @@ class TextEditor(Widget):
 
     @property
     def value(self):
-        """ The value of the multi line text input field.
+        """ The value of the editor's text
 
         Returns:
-            The text of the Widget as a ``str``.
+            The text of the editor as a ``str``.
         """
         return self._impl.get_value()
 
@@ -87,7 +94,7 @@ class TextEditor(Widget):
         self._impl.rehint()
 
     def clear(self):
-        """ Clears the text from the widget.
+        """ Clears the text from the editor
         """
         self.value = ''
 
